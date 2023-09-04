0392a1583043efd844709a4b2db98465f1c0b956
Aigar Voog <aigar.voog@centria.fi>
2023-09-04 20:00:51 +0300

    UI main page adjustmets

---
+++ src/components/IngredientList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="ingredient-list">
+        <span v-for="ingredient in ingredients" :key="ingredient">
+            {{ ingredient }}
+            <button @click="removeIngredient(ingredient)">x</button>
+        </span>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: ['ingredients'],
+    methods: {
+        removeIngredient(ingredient) {
+            this.$emit('remove-ingredient', ingredient);
+        },
+    }
+}
+</script>
+  
+<style scoped>
+.ingredient-list span {
+    margin: 5px;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+</style>
